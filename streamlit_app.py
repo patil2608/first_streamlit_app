4822bcccd93940d270312b52216bb6fe0ff18e40
patil2608 <116810274+patil2608@users.noreply.github.com>
2022-10-27 18:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
-import streamlit 
+import streamlit 🥣 🥗 🐔 🥑🍞
 
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast ')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
