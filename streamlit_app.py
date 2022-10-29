467f619f207071c772c08a266ddc3b04eea71149
patil2608 <116810274+patil2608@users.noreply.github.com>
2022-10-29 20:15:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())#just writes the data to the screen.
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kivi")
 
 
 streamlit.header("Fruityvice Fruit Advice!")
